c54cc1f4d131a8c1046881b2a956bcc9c60ff9da
tavisca-nnarkhede <51942815+tavisca-nnarkhede@users.noreply.github.com>
2019-06-21 23:33:01 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -22,7 +22,44 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
+            string a=equation.Split('*')[0];
+            string b=equation.Split('*')[1].Split('=')[0];
+            string c=equation.Split('=')[1];
+            int result=0;
+            var ans=0.0;
+            String ans1=null, ans2=null;
+            if (a.Contains("?"))
+            {
+                ans=double.Parse(c) / double.Parse(b);
+                ans1=a;
+            }
+            else if (b.Contains("?"))
+            {
+                ans=double.Parse(c) / double.Parse(a);
+                ans1=b;
+            }
+            else if (c.Contains("?"))
+            {
+                ans=double.Parse(a) * double.Parse(b);
+                ans1=c;
+            }
+            ans2=ans.ToString();
+            if (ans1.Length==ans2.Length)
+            {
+                for (int i=0;i<ans2.Length;i++)
+                {
+                    if (ans1[i]!=ans2[i])
+                    {
+                        result=int.Parse(ans2[i].ToString());
+                        return result;
+                    }
+
+                }
+            }
+            else
+            {
+                return -1;
+            }
             throw new NotImplementedException();
         }
     }
